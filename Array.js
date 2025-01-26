996ad4dd46c49811634a63f3af71091ddaa7d7d2
Sumon <sumonete152@gmail.com>
2025-01-27 01:53:14 +0600

    sumin

---
+++ Array.js
@@ -0,0 +1,22 @@
+let myArray = [2,6,8,2,8,9,10,5,7]
+// let reverseMyArray = myArray.reverse() // reverses the elements and remember that this opreration will change the original array
+const myHeroes = new Array("Muhammad","Walid","Ali")
+// myHeroes.push("Usman")
+// myHeroes.pop()
+// myArray.unshift(8) //will add element at the beginning and time consuming operation
+// myArray.shift()  //will remove the first Element
+// console.log(myArray.includes(2)) // return boolean
+// console.log(myArray.indexOf(9)); // return the value with associated index v
+// let whatIsJoin = myArray.join() // return the array's values in string formate
+// console.log(myHeroes);
+console.log(myArray);
+let sliceOpr = myArray.slice(1,3)
+console.log(sliceOpr);
+console.log(myArray);
+let spliceOpr = myArray.splice(1,3)
+console.log(spliceOpr)
+console.log(myArray);
+// splice change the original array and take the end position element
+
+
+ 
--- letConst.js
+++ letConst.js
@@ -2,6 +2,6 @@ const id = 1012;
 let name = "Sumon";
 // id = 1200; not allowed , can't change const value 
 name = "Islam"; // can be changed in local scope
-console.table(id,name);
+console.table([id,name]);
 
 
